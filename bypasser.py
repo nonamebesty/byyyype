4164e5d69acabc0f204d130348a2c25fa4006bfd
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-04-22 17:08:29 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1664,7 +1664,7 @@ def tnshort(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://usanewstoday.club/"
+    ref = "https://knowstuff.in/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
